1f16c161393472b333cd1a805ebced8ef70e6e78
Troy Larson <1931732+troylar@users.noreply.github.com>
2019-12-08 09:07:52 -0500

    Update iam_cloudsploit_role.tf

--- iam_cloudsploit_role.tf
+++ iam_cloudsploit_role.tf
@@ -28,7 +28,6 @@ EOF
 resource "aws_iam_role_policy_attachment" "cloudsploit_cross_account_attach" {
   # disable this if use_aws_gov == true
   count = "${var.use_aws_gov ? 0 : 1}"
-
-  role       = "${aws_iam_role.cloudsploit_cross_account_role.name}"
+  role       = "${aws_iam_role.cloudsploit_cross_account_role[count.index].name}" 
   policy_arn = "arn:aws:iam::aws:policy/SecurityAudit"
 }
